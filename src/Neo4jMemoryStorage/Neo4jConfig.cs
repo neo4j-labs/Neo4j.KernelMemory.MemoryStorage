23161bc0c7c49f7ac9c6ad60ea47ae4b108b0292
Grant Lodge <6323995+thelonelyvulpes@users.noreply.github.com>
2024-03-27 22:26:23 +0000

    Use .NET Conventions

--- src/Neo4jMemoryStorage/Neo4jConfig.cs
+++ src/Neo4jMemoryStorage/Neo4jConfig.cs
@@ -3,20 +3,19 @@
 public class Neo4jConfig
 {
     /// <summary>
-    /// Uri for connecting to Neo4j.
-    /// Default is "neo4j://localhost:7687"
+    ///     Uri for connecting to Neo4j.
+    ///     Default is "neo4j://localhost:7687"
     /// </summary>
     public string Uri { get; set; } = "neo4j://localhost:7687";
 
     /// <summary>
-    /// Username required to connect to Neo4j. 
-    /// Dedault is "neo4j"
+    ///     Username required to connect to Neo4j.
+    ///     Default is "neo4j"
     /// </summary>
     public string Username { get; set; } = "neo4j";
 
     /// <summary>
-    /// Password for authenticating username with Neo4j.
+    ///     Password for authenticating username with Neo4j.
     /// </summary>
     public string Password { get; set; } = string.Empty;
-
 }
--- src/Neo4jMemoryStorage/Neo4jMemory.cs
+++ src/Neo4jMemoryStorage/Neo4jMemory.cs
@@ -1,37 +1,31 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.CompilerServices;
+﻿using System.Runtime.CompilerServices;
 using System.Text;
-using System.Text.Json;
 using System.Text.RegularExpressions;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Globalization;
 using Microsoft.Extensions.Logging;
 using Microsoft.KernelMemory;
 using Microsoft.KernelMemory.AI;
 using Microsoft.KernelMemory.Diagnostics;
 using Microsoft.KernelMemory.MemoryStorage;
-
 using Neo4j.Driver;
 
 namespace Neo4j.KernelMemory.MemoryStorage;
 
 /// <summary>
-/// Basic vector db implementation, designed for tests and demos only.
-/// When searching, uses brute force comparing against all stored records.
+///     Basic vector db implementation, designed for tests and demos only.
+///     When searching, uses brute force comparing against all stored records.
 /// </summary>
 public class Neo4jMemory : IMemoryDb
 {
+    private const string ValidSeparator = "-";
+
+    // Note: normalize "_" to "-" for consistency with other DBs
+    private static readonly Regex ReplaceIndexNameCharsRegex = new(@"[\s|\\|/|.|_|:]", RegexOptions.Compiled);
+    private readonly IDriver _driver;
     private readonly ITextEmbeddingGenerator _embeddingGenerator;
     private readonly ILogger<Neo4jMemory> _log;
 
-    private readonly IDriver _driver;
-
     /// <summary>
-    /// Create new instance
+    ///     Create new instance
     /// </summary>
     /// <param name="config">Neo4j connection settings</param>
     /// <param name="embeddingGenerator">Text embedding generator</param>
@@ -41,16 +35,13 @@ public class Neo4jMemory : IMemoryDb
         ITextEmbeddingGenerator embeddingGenerator,
         ILogger<Neo4jMemory>? log = null)
     {
-        this._embeddingGenerator = embeddingGenerator;
+        _embeddingGenerator = embeddingGenerator;
 
-        if (this._embeddingGenerator == null)
-        {
-            throw new Neo4jException("Embedding generator not configured");
-        }
+        if (_embeddingGenerator == null) throw new Neo4jException("Embedding generator not configured");
 
-        this._log = log ?? DefaultLogger<Neo4jMemory>.Instance;
+        _log = log ?? DefaultLogger<Neo4jMemory>.Instance;
 
-        this._driver = Neo4jDriverFactory.BuildDriver(config, this._log);
+        _driver = Neo4jDriverFactory.BuildDriver(config, _log);
 
         Console.WriteLine($"Neo4jMemory created for {config.Uri}");
     }
@@ -63,24 +54,23 @@ public class Neo4jMemory : IMemoryDb
         var propertyKey = PropertyKeyForIndex(index);
 
         // Create a new index in Neo4j
-        return this._driver.ExecutableQuery($$"""
-            CREATE VECTOR INDEX `{{index}}` IF NOT EXISTS
-                FOR (m:{{label}}) ON (m.{{propertyKey}}) 
-                OPTIONS { indexConfig: {
-                    `vector.dimensions`: $vectorSize,
-                    `vector.similarity_function`: 'cosine'
-                    }
-                }
-            """
-        )
-        .WithParameters(new { vectorSize }).ExecuteAsync(cancellationToken);
-
+        return _driver.ExecutableQuery($$"""
+                                         CREATE VECTOR INDEX `{{index}}` IF NOT EXISTS
+                                             FOR (m:{{label}}) ON (m.{{propertyKey}})
+                                             OPTIONS { indexConfig: {
+                                                 `vector.dimensions`: $vectorSize,
+                                                 `vector.similarity_function`: 'cosine'
+                                                 }
+                                             }
+                                         """
+            )
+            .WithParameters(new { vectorSize }).ExecuteAsync(cancellationToken);
     }
 
     /// <inheritdoc />
     public async Task<IEnumerable<string>> GetIndexesAsync(CancellationToken cancellationToken = default)
     {
-        var response = await this._driver.ExecutableQuery($"SHOW VECTOR INDEXES YIELD name")
+        var response = await _driver.ExecutableQuery("SHOW VECTOR INDEXES YIELD name")
             .ExecuteAsync(cancellationToken).ConfigureAwait(false);
 
         var vectorIndexes = response.Result.ToList().Select(x => x["name"].As<string>());
@@ -94,38 +84,40 @@ public class Neo4jMemory : IMemoryDb
         var label = LabelForIndex(index);
         var propertyKey = PropertyKeyForIndex(index);
         // Create a new index in Neo4j
-        return this._driver.ExecutableQuery($$"""
-            CREATE VECTOR INDEX `{{index}}` IF NOT EXISTS
-                FOR (c:{{label}}) ON (c.{{propertyKey}}) 
-                OPTIONS { indexConfig: {
-                    `vector.dimensions`: $vectorSize,
-                    `vector.similarity_function`: 'cosine'
-                    }
-                }
-            """
-        )
-        .ExecuteAsync(cancellationToken);
+        return _driver.ExecutableQuery($$"""
+                                         CREATE VECTOR INDEX `{{index}}` IF NOT EXISTS
+                                             FOR (c:{{label}}) ON (c.{{propertyKey}})
+                                             OPTIONS { indexConfig: {
+                                                 `vector.dimensions`: $vectorSize,
+                                                 `vector.similarity_function`: 'cosine'
+                                                 }
+                                             }
+                                         """
+            )
+            .ExecuteAsync(cancellationToken);
     }
 
     /// <inheritdoc />
-    public async Task<string> UpsertAsync(string index, MemoryRecord record, CancellationToken cancellationToken = default)
+    public async Task<string> UpsertAsync(string index, MemoryRecord record,
+        CancellationToken cancellationToken = default)
     {
         index = NormalizeIndexName(index);
         var label = LabelForIndex(index);
         var propertyKey = PropertyKeyForIndex(index);
 
-        var response = await this._driver
+        var response = await _driver
             .ExecutableQuery($$"""
-                MERGE (m:Memory:{{label}} {id: $recordId}) 
-                SET m += $payload
-                WITH m
-                CALL db.create.setNodeVectorProperty(m, $propertyKey, $vector)  
-                RETURN m.id as recordId
-            """)
-            .WithParameters(new { 
-                recordId = record.Id, 
+                                   MERGE (m:Memory:{{label}} {id: $recordId})
+                                   SET m += $payload
+                                   WITH m
+                                   CALL db.create.setNodeVectorProperty(m, $propertyKey, $vector)
+                                   RETURN m.id as recordId
+                               """)
+            .WithParameters(new
+            {
+                recordId = record.Id,
                 payload = record.Payload,
-                propertyKey = propertyKey,
+                propertyKey,
                 vector = record.Vector.Data.ToArray()
             })
             .ExecuteAsync(cancellationToken).ConfigureAwait(false);
@@ -145,44 +137,42 @@ public class Neo4jMemory : IMemoryDb
         bool withEmbeddings = false,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        if (limit <= 0) { limit = int.MaxValue; }
+        if (limit <= 0) limit = int.MaxValue;
 
         index = NormalizeIndexName(index);
         var propertyKey = PropertyKeyForIndex(index);
 
-        var vector = await _embeddingGenerator.GenerateEmbeddingAsync(text)
-                                                            .ConfigureAwait(false);
-        
-        var queryResult = await this._driver
-            .ExecutableQuery($$"""
-            CALL db.index.vector.queryNodes($indexName, $topK, $vector) 
-            YIELD node, score
-            RETURN score, node.id as recordId, properties(node) as payload, node[$propertyKey] as vector
-        """)
-            .WithParameters(new { 
+        var vector = await _embeddingGenerator.GenerateEmbeddingAsync(text, cancellationToken)
+            .ConfigureAwait(false);
+
+        var queryResult = await _driver
+            .ExecutableQuery("""
+                                 CALL db.index.vector.queryNodes($indexName, $topK, $vector)
+                                 YIELD node, score
+                                 RETURN score, node.id as recordId, properties(node) as payload, node[$propertyKey] as vector
+                             """)
+            .WithParameters(new
+            {
                 indexName = index,
                 topK = limit,
-                propertyKey = propertyKey,
+                propertyKey,
                 vector = vector.Data.ToArray()
             })
             .ExecuteAsync(cancellationToken).ConfigureAwait(false);
 
         var memories = queryResult.Result.Select(memory => (
-            new MemoryRecord()
-                {
-                    Id = memory["recordId"].As<string>(),
-                    Payload = memory["payload"].As<Dictionary<string, object>>(),
-                    Vector = memory["vector"].As<List<float>>().ToArray()
-                },
-            memory["score"].As<double>()
+                    new MemoryRecord
+                    {
+                        Id = memory["recordId"].As<string>(),
+                        Payload = memory["payload"].As<Dictionary<string, object>>(),
+                        Vector = memory["vector"].As<List<float>>().ToArray()
+                    },
+                    memory["score"].As<double>()
+                )
             )
-        )
-        .ToList();
+            .ToList();
 
-        foreach (var memory in memories)
-        {
-            yield return memory;
-        }
+        foreach (var memory in memories) yield return memory;
     }
 
     /// <inheritdoc />
@@ -193,14 +183,14 @@ public class Neo4jMemory : IMemoryDb
         bool withEmbeddings = false,
         [EnumeratorCancellation] CancellationToken cancellationToken = default)
     {
-        if (limit <= 0) { limit = int.MaxValue; }
+        if (limit <= 0) limit = int.MaxValue;
 
         index = NormalizeIndexName(index);
 
         // Get similar records from Neo4j
         await Task.Delay(0, cancellationToken).ConfigureAwait(false);
 
-        yield return (new MemoryRecord());
+        yield return new MemoryRecord();
     }
 
     /// <inheritdoc />
@@ -211,20 +201,13 @@ public class Neo4jMemory : IMemoryDb
         return Task.CompletedTask;
     }
 
-    #region private
-
-    // Note: normalize "_" to "-" for consistency with other DBs
-    private static readonly Regex s_replaceIndexNameCharsRegex = new(@"[\s|\\|/|.|_|:]");
-    private const string ValidSeparator = "-";
 
     private static string NormalizeIndexName(string index)
     {
         if (string.IsNullOrWhiteSpace(index))
-        {
             throw new ArgumentNullException(nameof(index), "The index name is empty");
-        }
 
-        index = s_replaceIndexNameCharsRegex.Replace(index.Trim().ToLowerInvariant(), ValidSeparator);
+        index = ReplaceIndexNameCharsRegex.Replace(index.Trim().ToLowerInvariant(), ValidSeparator);
 
         return index.Trim();
     }
@@ -242,24 +225,21 @@ public class Neo4jMemory : IMemoryDb
 
     private static bool TagsMatchFilters(TagCollection tags, ICollection<MemoryFilter>? filters)
     {
-        if (filters == null || filters.Count == 0) { return true; }
+        if (filters == null || filters.Count == 0) return true;
 
         // Verify that at least one filter matches (OR logic)
-        foreach (MemoryFilter filter in filters)
+        foreach (var filter in filters)
         {
             var match = true;
 
             // Verify that all conditions are met (AND logic)
-            foreach (KeyValuePair<string, List<string?>> condition in filter)
-            {
+            foreach (var condition in filter)
                 // Check if the tag name + value is present
-                for (int index = 0; match && index < condition.Value.Count; index++)
-                {
-                    match = match && (tags.ContainsKey(condition.Key) && tags[condition.Key].Contains(condition.Value[index]));
-                }
-            }
+                for (var index = 0; match && index < condition.Value.Count; index++)
+                    match = match && tags.ContainsKey(condition.Key) &&
+                            tags[condition.Key].Contains(condition.Value[index]);
 
-            if (match) { return true; }
+            if (match) return true;
         }
 
         return false;
@@ -276,6 +256,4 @@ public class Neo4jMemory : IMemoryDb
         var bytes = Convert.FromBase64String(encodedId.Replace('_', '='));
         return Encoding.UTF8.GetString(bytes);
     }
-
-    #endregion
 }
